e6e645d7b835f9caffb10487bce480f62f730798
Buddhi Kavindra <visitbudkavin@gmail.com>
2022-09-13 01:35:10 +0530

    adding scrapper code

---
+++ scrapper.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://en.wikipedia.org/wiki/Elizabeth_II'
+
+result = requests.get(url)
+
+doc = BeautifulSoup(result.text, 'html.parser')
+
+content = doc.find("div", {"id": "mw-content-text"})
+
+# print(content.prettify())
+
+# print(content.text)
+
+# print(content.encode("utf-8"))
+
+with open('dist/content.txt', mode="w", encoding="utf-8") as f:
+    f.write(content.text)
